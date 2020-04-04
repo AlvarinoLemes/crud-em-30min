d724e382758cbc04e94c098b937ec0b41a7e3a4c
Anonimo <anonimo@Anonimo.local>
2020-04-04 17:52:56 -0300

    teste

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     description = models.CharField(max_length=100)
-    price = models.Decimal(max_digits=9, decimal_places=2)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
     quantity = models.IntegerField()
 
     def __str__(self):
--- products/views.py
+++ products/views.py
@@ -5,3 +5,12 @@ from .forms import ProductForm
 def list_products(request):
     products = Product.objects.all()
     return render(request, 'products.html', {'products': products})
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_products')
+
+    return render(request, 'products-form.html', {'form': form})
