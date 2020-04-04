52558d69deee693e0728e5098262e6e94380f048
Anonimo <anonimo@Anonimo.local>
2020-04-04 18:35:53 -0300

    new

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from .views import list_products, create_product
 
 urlpatterns = [
     path('', list_products, name='list_products'),
-    path('new', create_product, name='create_products'),
+    path('new', create_product, name='create_product'),
 ]
